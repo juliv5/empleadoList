97b41ae928c7c5f946ac86d22fb4bcc2ed12d9d6
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-19 00:58:28 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { EmpleadoListComponent } from './components/empleado-list/empleado-list.component';
 import { CountEmpleadosComponent } from './components/empleado-list/count-empleados/count-empleados.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoListComponent,
-    CountEmpleadosComponent
+    CountEmpleadosComponent,
+    
   ],
   imports: [
     BrowserModule,
